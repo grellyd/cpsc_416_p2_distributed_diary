77da90b01a123ea6b0cee1f27779299527217125
Alex Budkina <f1l0b@ugrad.cs.ubc.ca>
2018-03-13 15:54:15 -0700

    Added: interface for acceptor

--- go/src/consensuslib/paxosnode/acceptor/acceptor.go
+++ go/src/consensuslib/paxosnode/acceptor/acceptor.go
@@ -1,17 +1,32 @@
 package acceptor
 
 import (
-	"proj2/consensuslib/paxosnode"
+	"proj2_c6y8_f1l0b_l0j8_l5w8_n5w8/go/src/consensuslib"
 )
 
-type Acceptor struct {
-	LastPromised Message
-	LastAccepted Message
+
+type AcceptorRole struct {
+	LastPromised consensuslib.Message
+	LastAccepted consensuslib.Message
 }
 
-func NewAcceptor() Acceptor {
-	acc := Acceptor{nil, nil}
+func NewAcceptor() AcceptorRole {
+	acc := AcceptorRole{nil, nil}
 	return acc
 }
+
+type AcceptorInterface interface {
+
+	// REQUIRES: a message with the empty/nil/'' string as a value;
+	// EFFECTS: responds with the latest promised/accepted message or with the nil if none
+	processPrepare (msg consensuslib.Message) consensuslib.Message
+
+	// REQUIRES: a message with a value submitted at proposer;
+	// EFFECTS: responds with the latest promised/accepted message or with the nil if none
+	processAccept (msg consensuslib.Message) consensuslib.Message
+
+}
+
+
 
 
