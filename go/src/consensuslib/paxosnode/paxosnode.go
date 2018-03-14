550e416d6273a123a569d2f7b9d7256420e65d1d
Sharon Yang <l5w8@bowen.ugrad.cs.ubc.ca>
2018-03-14 11:42:58 -0700

    Added more methods to interface

--- go/src/consensuslib/paxosnode/paxosnode.go
+++ go/src/consensuslib/paxosnode/paxosnode.go
@@ -24,17 +24,21 @@ type PaxosNodeInterface interface {
 	BecomeNeighbours(ips []string) (connectedNbrs []string, err error)
 
 	// Handles the entire process of proposing a value and trying to achieve consensus
-	//TODO[]sharon: update parameters as needed.
+	//TODO[sharon]: update parameters as needed. Might be RPC
 	ProposeValue(value string) (success bool, err error)
 
 	// Sends the value that consensus has been reached on to the entire network.
 	// Must be called after ProposeValue has returned successfully
-	//TODO[sharon]: Figure out best name for number field and add as param
+	//TODO[sharon]: Figure out best name for number field and add as param. Might be RPC
 	DisseminateAcceptedValue(value string) (success bool, err error)
 
 	// Locally accepts the accept request sent by a PN in the system.
-	// TODO[sharon]: Figure out parameters
+	// TODO[sharon]: Figure out parameters. Might be RPC
 	AcceptAcceptRequest() (err error)
+
+	// Sends a prepare request to all neighbours on behalf of the Paxosnode's proposer
+	// TODO[sharon]: Check parameters that get passed in
+	PSendPrepareRequest(value string) (err error)
 
 	// Exits the Paxosnode network.
 	LeaveNetwork()
