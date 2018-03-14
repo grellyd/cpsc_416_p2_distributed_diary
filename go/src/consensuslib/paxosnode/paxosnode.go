40201dabafcae56ca5abdde32eedc1d1d0ef5b63
Sharon Yang <l5w8@bowen.ugrad.cs.ubc.ca>
2018-03-14 11:23:29 -0700

    Added PaxosNodeInterface and functions

--- go/src/consensuslib/paxosnode/paxosnode.go
+++ go/src/consensuslib/paxosnode/paxosnode.go
@@ -1,9 +1,9 @@
 package paxosnode
 
 import (
-	"consensuslib/paxosnode/proposer";
-	"consensuslib/paxosnode/acceptor";
+	"consensuslib/paxosnode/acceptor"
 	"consensuslib/paxosnode/learner"
+	"consensuslib/paxosnode/proposer"
 )
 
 type ProposerRole = proposer.ProposerRole
@@ -11,13 +11,31 @@ type AcceptorRole = acceptor.AcceptorRole
 type LearnerRole = learner.LearnerRole
 
 type PaxosNode struct {
-	Addr			 string // IP:port, identifier
+	Addr       string // IP:port, identifier
 	Proposer   ProposerRole
 	Acceptor   AcceptorRole
 	Learner    LearnerRole
+	NbrAddrs   []string
 	Neighbours map[string]*rpc.client
 }
 
 type PaxosNodeInterface interface {
+	// Sets up bidirectional RPC with all neighbours, given to the paxosnode by the client
+	BecomeNeighbours(ips []string) (connectedNbrs []string, err error)
 
+	// Handles the entire process of proposing a value and trying to achieve consensus
+	//TODO[]sharon: update parameters as needed.
+	ProposeValue(value string) (success bool, err error)
+
+	// Sends the value that consensus has been reached on to the entire network.
+	// Must be called after ProposeValue has returned successfully
+	//TODO[sharon]: Figure out best name for number field and add as param
+	DisseminateAcceptedValue(value string) (success bool, err error)
+
+	// Locally accepts the accept request sent by a PN in the system.
+	// TODO[sharon]: Figure out parameters
+	AcceptAcceptRequest() (err error)
+
+	// Exits the Paxosnode network.
+	LeaveNetwork()
 }
