ce243edf90cb8a39615c3cda3d7e6bfe722aa3b4
grellyd <graham.brown137@gmail.com>
2018-03-20 14:36:22 -0700

    Working go install commands

---
+++ go/src/distributeddiaryapp/app.go
@@ -0,0 +1,15 @@
+// Entrypoint for the Distributed Diary Application
+// This file can be run with 'go run distributeddiaryapp/app.go'
+// Or do `cd distributeddiaryapp && go build && ./distributeddiaryapp`
+// Or do `go install` then `distributeddiaryapp` to run the binary
+// The last is @grellyd preferred for ease, but requires you to add `go/bin` to your $PATH variable
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("testing app")
+}
---
+++ go/src/distributeddiaryserver/server.go
@@ -0,0 +1,15 @@
+// Entrypoint for the Distributed Diary Server
+// This file can be run with 'go run distributeddiaryserver/server.go'
+// Or do `cd distributeddiaryserver && go build && ./distributeddiaryserver`
+// Or do `go install` then `distributeddiaryserver` to run the binary
+// The last is @grellyd preferred for ease, but requires you to add `go/bin` to your $PATH variable
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("testing server")
+}
