bce6520ca8382867d37eec40f7a05796846ae48d
Larissa Feng <hello@larissafeng.me>
2018-04-02 22:09:14 -0700

    Parameterize args to Command

--- go/src/distributeddiaryapp/networking/networking.go
+++ go/src/distributeddiaryapp/networking/networking.go
@@ -7,7 +7,7 @@ import (
 
 // GetOutboundIP Returns a machine's public (outbound) IP address e.g. "270.0.21.1".
 func GetOutboundIP() (ipString string, err error) {
-	out, err := exec.Command("curl -s http://checkip.amazonaws.com || printf \"0.0.0.0\"").Output()
+	out, err := exec.Command("curl", "-s", "http://checkip.amazonaws.com").Output()
 
 	if err != nil {
 		fmt.Println(err)
